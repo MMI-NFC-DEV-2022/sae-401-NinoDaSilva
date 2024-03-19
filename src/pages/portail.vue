9b9ccdafde3722fac920303e279aa830d3bc358b
Nino DA SILVA <ninoportos@gmail.com>
2024-03-19 18:46:28 +0100

    create client space

---
+++ src/pages/portail.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { user, doLogout } from '@/supabase'
+import { useRouter } from 'vue-router'
+
+// Redirige l'utilisateur vers la page de connexion si il n'est pas connecté
+const router = useRouter()
+if (!user) {
+    router.push('/connexion')
+}
+</script>
+
+<template>
+    <div v-if="user" class="text-center">
+        <h2 class="">Bonjour</h2>
+        <div class="mt-6">
+        <button type="submit" @click="doLogout"
+            class="py-2 px-10 sm:px-16 rounded-full shadow-md hover:scale-[1.02] duration-300">
+            Déconnexion
+        </button>
+        </div>
+    </div>
+</template>
