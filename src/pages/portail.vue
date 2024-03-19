3623245ce46e54f216cd35ea13be641c09de9ef7
Nino DA SILVA <ninoportos@gmail.com>
2024-03-19 19:48:32 +0100

    correction logout

--- src/pages/portail.vue
+++ src/pages/portail.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { user, doLogout } from '@/supabase'
+import { user, supabase } from '@/supabase'
 import { useRouter } from 'vue-router'
 
 // Redirige l'utilisateur vers la page de connexion si il n'est pas connecté
@@ -13,7 +13,7 @@ if (!user) {
     <div v-if="user" class="text-center">
         <h2 class="">Bonjour</h2>
         <div class="mt-6">
-        <button type="submit" @click="doLogout"
+        <button @pointerdown="supabase.auth.signOut()"
             class="py-2 px-10 sm:px-16 rounded-full shadow-md hover:scale-[1.02] duration-300">
             Déconnexion
         </button>
