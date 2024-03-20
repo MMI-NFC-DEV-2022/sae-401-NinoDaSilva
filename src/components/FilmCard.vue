d3c77612acf104d6155dfb3fc33675a8d7bba9d3
Nino DA SILVA <ninoportos@gmail.com>
2024-03-20 21:04:23 +0100

    correction carrousel

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -13,24 +13,30 @@ const fetchFilms = async () => {
 
 onMounted(fetchFilms)
 
+// Fonction de gestion du défilement du carousel
 const carousel = ref();
-  const prevSlide = () => {
-  const carouselWidth = carousel.value.offsetWidth;
-  carousel.value.scrollLeft -= carouselWidth;
-};
+
+const prevSlide = () => {
+  if (!carousel.value) return
+  const slideWidth = carousel.value.clientWidth
+  carousel.value.scrollLeft -= slideWidth
+}
 
 const nextSlide = () => {
-  const carouselWidth = carousel.value.offsetWidth;
-  carousel.value.scrollLeft += carouselWidth;
-};
+  if (!carousel.value) return
+  const slideWidth = carousel.value.clientWidth
+  carousel.value.scrollLeft += slideWidth
+}
 </script>
 
 <template>
   <div>
     <h1 class="font-bold mb-5 text-3xl sm:text-4xl">Les films du moment</h1>
-    <div class="carousel" ref="carousel">
-      <button @click="prevSlide" class="bouton-carousel bouton-carousel--left max-sm:hidden">Précédent</button>
-      <div class="carousel-parent-slide">
+    <div class="carousel">
+      <button @click="prevSlide" class="bouton-carousel bouton-carousel--left max-sm:hidden" aria-label="slide precedente">
+        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24"><g transform="rotate(-90 12 12)"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m17 14l-5-5m0 0l-5 5"/></g></svg>
+      </button>
+      <div class="carousel-parent-slide" ref="carousel">
         <div class="carousel-slide" v-for="film in films" :key="film.id">
           <RouterLink :to="`/films/${film.id}`">
             <img :src="film.affiche_film" alt="Affiche du film" class="carousel-image" />
@@ -40,7 +46,9 @@ const nextSlide = () => {
           </RouterLink>
         </div>
       </div>
-      <button @click="nextSlide" class="bouton-carousel bouton-carousel--right max-sm:hidden">Suivant</button>
+      <button @click="nextSlide" class="bouton-carousel bouton-carousel--right max-sm:hidden" aria-label="slide suivante">
+        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24"><g transform="rotate(90 12 12)"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m17 14l-5-5m0 0l-5 5"/></g></svg>
+      </button>
     </div>
   </div>
 </template>
--- src/pages/collections/[id].vue
+++ src/pages/collections/[id].vue
@@ -15,15 +15,19 @@ if (error) {
   <div>
     <h2 class="font-bold text-3xl sm:text-4xl">{{ collectionData.nom_collection }}</h2>
 
-    <div class="carousel">
-      <div class="carousel-inner">
-        <div v-for="film in collectionData.film_collection" :key="film.id" class="carousel-item">
+    <div class="carousel" ref="carousel">
+      <button @click="prevSlide" class="bouton-carousel bouton-carousel--left max-sm:hidden">Précédent</button>
+      <div class="carousel-parent-slide">
+        <div class="carousel-slide" v-for="film in collectionData.film_collection" :key="film.id">
           <RouterLink :to="`/films/${film.film.id}`">
             <img :src="film.film.affiche_film" alt="Affiche du film" class="carousel-image" />
-            <h2 class="carousel-title">{{ film.film.titre_film }}</h2>
+            <div class="carousel-content--title">
+              <h2 class="carousel-title">{{ film.film.titre_film }}</h2>
+            </div>
           </RouterLink>
         </div>
       </div>
+      <button @click="nextSlide" class="bouton-carousel bouton-carousel--right max-sm:hidden">Suivant</button>
     </div>
   </div>
 </template>
