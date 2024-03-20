5edc82fb04c79d7a7f8a745f98d66d4dd04ae942
Nino DA SILVA <ninoportos@gmail.com>
2024-03-20 21:06:24 +0100

    carousel collection

--- src/pages/collections/[id].vue
+++ src/pages/collections/[id].vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 import { supabase } from '@/supabase'
+import { ref } from 'vue'
 
 const route = useRoute()
 
@@ -9,15 +10,31 @@ const { data: collectionData, error } = await supabase.from('collection').select
 if (error) {
   console.error('Erreur lors du chargement des données :', error)
 }
+
+// Fonction de gestion du défilement du carousel
+const carousel = ref();
+
+const prevSlide = () => {
+  if (!carousel.value) return
+  const slideWidth = carousel.value.clientWidth
+  carousel.value.scrollLeft -= slideWidth
+}
+const nextSlide = () => {
+  if (!carousel.value) return
+  const slideWidth = carousel.value.clientWidth
+  carousel.value.scrollLeft += slideWidth
+}
 </script>
 
 <template>
   <div>
     <h2 class="font-bold text-3xl sm:text-4xl">{{ collectionData.nom_collection }}</h2>
 
-    <div class="carousel" ref="carousel">
-      <button @click="prevSlide" class="bouton-carousel bouton-carousel--left max-sm:hidden">Précédent</button>
-      <div class="carousel-parent-slide">
+    <div class="carousel mt-5">
+      <button @click="prevSlide" class="bouton-carousel bouton-carousel--left max-sm:hidden" aria-label="slide precedente">
+        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24"><g transform="rotate(-90 12 12)"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m17 14l-5-5m0 0l-5 5"/></g></svg>
+      </button>
+      <div class="carousel-parent-slide" ref="carousel">
         <div class="carousel-slide" v-for="film in collectionData.film_collection" :key="film.id">
           <RouterLink :to="`/films/${film.film.id}`">
             <img :src="film.film.affiche_film" alt="Affiche du film" class="carousel-image" />
@@ -27,7 +44,9 @@ if (error) {
           </RouterLink>
         </div>
       </div>
-      <button @click="nextSlide" class="bouton-carousel bouton-carousel--right max-sm:hidden">Suivant</button>
+      <button @click="nextSlide" class="bouton-carousel bouton-carousel--right max-sm:hidden" aria-label="slide suivante">
+        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24"><g transform="rotate(90 12 12)"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m17 14l-5-5m0 0l-5 5"/></g></svg>
+      </button>
     </div>
   </div>
 </template>
