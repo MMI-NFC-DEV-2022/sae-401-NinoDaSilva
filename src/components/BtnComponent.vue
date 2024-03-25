7f6c0b32a28bedbcdc6ae386a6cc80dce4996687
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-03-25 16:11:40 +0100

    update

--- src/components/BtnComponent.vue
+++ src/components/BtnComponent.vue
@@ -9,7 +9,7 @@ defineProps<{
 <template>
     <div class="text-center mt-5">
         <button type="submit" @click="!stateToggle"
-            class="py-2 px-10 sm:px-16 rounded-xl shadow-md hover:scale-[1.02] duration-300">
+            class="py-2 px-10 rounded-xl shadow-md hover:scale-[1.02] bg-noir hover:bg-orange hover:text-noir text-blanc duration-300">
             {{ buttonText }}
         </button>
     </div>
